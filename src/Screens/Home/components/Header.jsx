530f80b5e2bb1fbc2dc6aadcf9f1c9bbc07db1fe
Alek30k <alemank23k@gmail.com>
2024-05-25 22:58:18 -0300

    last commit

--- src/Screens/Home/components/Header.jsx
+++ src/Screens/Home/components/Header.jsx
@@ -1,9 +1,12 @@
+import { Plus } from "lucide-react";
 import logo from "./../../../assets/AC.png";
 
 const Header = () => {
   return (
     <div className="flex justify-between items-center shadow-lg p-4 border rounded-lg">
-      <button className="btn btn-primary btn-sm md:btn-md">+ New Idea</button>
+      <button className="btn btn-primary btn-sm md:btn-md">
+        <Plus className="h-6 w-6" /> New Idea
+      </button>
       <h2 className="font-bold text-sm md:text-2xl">Top 20 Ideas</h2>
       <div className="flex gap-2 items-center">
         <img src={logo} alt="logo" className="w-10 h-10 rounded-full" />
--- src/Screens/newIdea/AddNewScreen.jsx
+++ src/Screens/newIdea/AddNewScreen.jsx
@@ -1,3 +1,4 @@
+import { ChevronLeft, Info, Send } from "lucide-react";
 import Header from "../Home/components/Header";
 
 const AddNewScreen = () => {
@@ -5,7 +6,10 @@ const AddNewScreen = () => {
     <div className="">
       <Header />
 
-      <button className="btn mt-7">Back</button>
+      <button className="btn mt-7">
+        <ChevronLeft />
+        Back
+      </button>
       <h2 className="font-bold text-2xl mt-5">
         From Concept to Creation : Empowering your startup Journey
       </h2>
@@ -13,13 +17,14 @@ const AddNewScreen = () => {
         <label>Your Idea *</label>
         <textarea
           className="textarea textarea-bordered border-primary"
-          placeholder="Bio"
+          placeholder="Write your Idea"
         ></textarea>
       </div>
       <div className="flex flex-col mt-7 gap-2">
         <label className="flex justify-between">
           Your Username *
           <span className="flex items-center gap-2 text-sm">
+            <Info className="h-4 w-4" />
             No Account Needed
           </span>
         </label>
@@ -29,6 +34,10 @@ const AddNewScreen = () => {
           className="input input-bordered w-full border-primary"
         />
       </div>
+      <button className="btn w-full btn-primary mt-7">
+        Send
+        <Send className="h-4 w-4" />
+      </button>
     </div>
   );
 };
