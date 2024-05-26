83020e1c958e0a11cefc2b271ded02a21e41759a
Alek30k <alemank23k@gmail.com>
2024-05-25 22:40:44 -0300

    last commit

--- src/Screens/newIdea/AddNewScreen.jsx
+++ src/Screens/newIdea/AddNewScreen.jsx
@@ -9,8 +9,25 @@ const AddNewScreen = () => {
       <h2 className="font-bold text-2xl mt-5">
         From Concept to Creation : Empowering your startup Journey
       </h2>
-      <div className="">
-        <label>Your Idea</label>
+      <div className="flex flex-col mt-7 gap-2">
+        <label>Your Idea *</label>
+        <textarea
+          className="textarea textarea-bordered border-primary"
+          placeholder="Bio"
+        ></textarea>
+      </div>
+      <div className="flex flex-col mt-7 gap-2">
+        <label className="flex justify-between">
+          Your Username *
+          <span className="flex items-center gap-2 text-sm">
+            No Account Needed
+          </span>
+        </label>
+        <input
+          type="text"
+          placeholder="Username"
+          className="input input-bordered w-full border-primary"
+        />
       </div>
     </div>
   );
