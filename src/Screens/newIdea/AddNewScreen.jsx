396e44cbf32ba17a7eb93d79779f0e39a272eab1
Alek30k <alemank23k@gmail.com>
2024-05-26 23:01:55 -0300

    last commit

--- src/Screens/newIdea/AddNewScreen.jsx
+++ src/Screens/newIdea/AddNewScreen.jsx
@@ -3,23 +3,54 @@ import Header from "../Home/components/Header";
 import { useState } from "react";
 import { db } from "../../../utils";
 import { Ideas } from "../../../utils/schema";
+import moment from "moment";
 
 const AddNewScreen = () => {
   const [idea, setIdea] = useState();
   const [username, setUsername] = useState();
+  const [showAlert, setShowAlert] = useState(false);
 
   const onSavehandler = async () => {
-    const result = await db.insert(Ideas).values({
-      content: idea,
-      username: username,
-      createdAt,
-    });
+    const result = await db
+      .insert(Ideas)
+      .values({
+        content: idea,
+        username: username,
+        createdAt: moment().format("DD MMM yyyy"),
+      })
+      .returning({ id: Ideas.id });
+
+    if (result) {
+      localStorage.setItem("username", username);
+      console.log("Insert data");
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 5000);
+    }
   };
 
   return (
     <div className="">
       <Header />
-
+      {showAlert && (
+        <div role="alert" className="alert alert-success mt-5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>Your purchase has been confirmed!</span>
+        </div>
+      )}
       <button className="btn mt-7">
         <ChevronLeft />
         Back
