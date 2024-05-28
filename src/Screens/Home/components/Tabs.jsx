8decfafa549074a2289cec9f9d300b300c470cf6
Alek30k <alemank23k@gmail.com>
2024-05-27 21:38:03 -0300

    last commit

--- src/Screens/Home/components/Tabs.jsx
+++ src/Screens/Home/components/Tabs.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+
 const Tabs = () => {
+  const [activeTab, setActiveTab] = useState();
+
   return (
     <div role="tablist" className="tabs tabs-bordered ">
-      <a role="tab" className="tab text-lg font-bold">
+      <a
+        role="tab"
+        href="/#hot"
+        onClick={() => setActiveTab(0)}
+        className={`tab text-lg font-bold ${activeTab == 0 && "tab-active"}`}
+      >
         🔥 Hot
       </a>
-      <a role="tab" className="tab text-lg font-bold tab-active">
+      <a
+        role="tab"
+        href="/#new"
+        onClick={() => setActiveTab(1)}
+        className={`tab text-lg font-bold ${activeTab == 1 && "tab-active"}`}
+      >
         ✨ New
       </a>
-      <a role="tab" className="tab text-lg font-bold">
+      <a
+        role="tab"
+        href="/#top"
+        onClick={() => setActiveTab(2)}
+        className={`tab text-lg font-bold ${activeTab == 2 && "tab-active"}`}
+      >
         🏆 Top
       </a>
     </div>
