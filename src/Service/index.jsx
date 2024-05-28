8076d890389bb6ebfd4113d6514f184594413003
Alek30k <alemank23k@gmail.com>
2024-05-28 15:33:12 -0300

    last commit

--- src/Service/index.jsx
+++ src/Service/index.jsx
@@ -1,46 +1,46 @@
-// export const upvote = (id) => {
-//   const votes = localStorage.getItem("votes")
-//     ? JSON.parse(localStorage.getItem("votes"))
-//     : {
-//         upvotes: [],
-//         downvotes: [],
-//       };
+export const upvote = (id) => {
+  const votes = localStorage.getItem("votes")
+    ? JSON.parse(localStorage.getItem("votes"))
+    : {
+        upvotes: [],
+        downvotes: [],
+      };
 
-//   if (votes.upvotes?.indexOf(id) !== -1) {
-//     return false;
-//   }
+  if (votes.upvotes?.indexOf(id) !== -1) {
+    return false;
+  }
 
-//   votes.upvotes?.push(id);
+  votes.upvotes?.push(id);
 
-//   const downVotes = votes.downvotes?.filter((item) => item != id);
-//   votes.downvotes = downVotes;
+  const downVotes = votes.downvotes?.filter((item) => item != id);
+  votes.downvotes = downVotes;
 
-//   localStorage.setItem("votes", JSON.stringify(votes));
+  localStorage.setItem("votes", JSON.stringify(votes));
 
-//   return true;
-// };
+  return true;
+};
 
-// export const downvote = (id) => {
-//   const votes = localStorage.getItem("votes")
-//     ? JSON.parse(localStorage.getItem("votes"))
-//     : {
-//         upvotes: [],
-//         downvotes: [],
-//       };
+export const downvote = (id) => {
+  const votes = localStorage.getItem("votes")
+    ? JSON.parse(localStorage.getItem("votes"))
+    : {
+        upvotes: [],
+        downvotes: [],
+      };
 
-//   if (votes.downvotes.indexOf(id) !== -1) {
-//     return false;
-//   }
+  if (votes.downvotes.indexOf(id) !== -1) {
+    return false;
+  }
 
-//   votes.downvotes.push(id);
+  votes.downvotes.push(id);
 
-//   const upVotes = votes.upvotes?.filter((item) => item != id);
-//   votes.upvotes = upVotes;
+  const upVotes = votes.upvotes?.filter((item) => item != id);
+  votes.upvotes = upVotes;
 
-//   localStorage.setItem("votes", JSON.stringify(votes));
+  localStorage.setItem("votes", JSON.stringify(votes));
 
-//   return true;
-// };
+  return true;
+};
 
 export const checkIsAlreadyUpVoted = (id) => {
   const votes = JSON.parse(localStorage.getItem("votes"));
