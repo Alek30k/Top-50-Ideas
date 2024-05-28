36195d19eb644a3a7de1543adcd4d7c96e453418
Alek30k <alemank23k@gmail.com>
2024-05-27 21:56:09 -0300

    last commit

--- src/Screens/Home/HomeScreen.jsx
+++ src/Screens/Home/HomeScreen.jsx
@@ -1,9 +1,29 @@
+import { useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Tabs from "./components/Tabs";
+import { db } from "./../../../utils/index";
+import { Ideas } from "../../../utils/schema";
+import { useEffect, useState } from "react";
+import { desc } from "drizzle-orm";
 
 const HomeScreen = () => {
-  const GetAllIdeas = () => {};
+  const params = useLocation();
+
+  const [ideasList, setIdeasList] = useState([]);
+
+  useEffect(() => {
+    GetAllIdeas();
+  }, [params]);
+
+  const GetAllIdeas = async () => {
+    const result = await db
+      .select()
+      .from(Ideas)
+      .orderBy(desc(params.hash == "#hot" ? Ideas.vote : Ideas.id))
+      .limit(20);
+    setIdeasList(result);
+  };
 
   return (
     <div className="">
